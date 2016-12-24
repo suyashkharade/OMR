18b7c28edb5638ab6293f0a2db15a199bebb8bb3
aradake <aradake>
2016-12-24 18:33:28 +0530

    first commit

---
+++ ImportData/src/file.java
@@ -0,0 +1,14 @@
+
+public class file {
+    public static void main(String[] args) {
+        System.out.print("**".length());
+        System.out.print("".length());
+        System.out.print("*".length());
+        System.out.print("*******".length());
+    }
+}
+
+
+
+
+
